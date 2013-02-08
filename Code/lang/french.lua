beb6353f53e1fb7097a9da9f02cbfa332ec20eb1
Banasura <banasura@ntui.fake>
2013-02-08 01:10:15 +0100

    Dev begining

--- Code/lang/french.lua
+++ Code/lang/french.lua
@@ -1,16 +1,6 @@
 --[[		NEVER TRUST USER'S INPUT non-released v0.1 alpha
 Software released under GNU Public General licence. See the "LICENSE" file to get more precisions.
 					Work made by SeaSharp's Team !
+-------------------------------------------------------------------------------------------------
+						French Language Pack
 ]]
-
-require 'zoetrope'
-require 'menu'
-require 'objects'
-require 'endScreen'
-
-the.app = App:new
-{
-	onRun = function (self)
-		self.view = Menu:new()
-	end
-}
