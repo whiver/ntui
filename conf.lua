25ca4efc4b3d46d8dbc6db6c37f39ee3aa9d8375
Joris "Aramos" Chavot <jorischavot@gmail.com>
2014-10-02 17:22:40 +0200

    Suppressions de tout les fichiers pour remise à neuf =D

--- conf.lua
+++
@@ -1,7 +0,0 @@
-function love.conf (t)
-	t.screen.width = 800
-	t.screen.height = 600
-	t.identity = "Never Trust User's Input - v0.01 alpha"
-	t.soundActivated = true
-	t.lang = 1 -- 1: French / 2: English
-end
